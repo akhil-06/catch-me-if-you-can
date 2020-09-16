166396a2057398b033ba8a001209769edb5218bf
Akhil Sharma <54715392+akhil-04@users.noreply.github.com>
2020-09-16 14:32:59 +0530

    Add files via upload

---
+++ catchme.js
@@ -0,0 +1,39 @@
+var box = document.getElementById('box');
+
+var viewWidth = window.innerWidth;
+var viewHeight = window.innerHeight;
+
+
+box.addEventListener("mouseover", function(event) {
+    var boxAttr = box.getBoundingClientRect();
+    console.log(boxAttr);
+    
+    var pos = getNewPosition(boxAttr.width, boxAttr.height);
+    console.log(pos);
+    
+    box.style.top = pos.y + "px";
+    box.style.left = pos.x + "px";
+});
+
+
+
+function getNewPosition(boxWidth, boxHeight) {
+    
+    // The boxWidth and boxHeight are subtracted so that they would not move out from the right and bottom direction
+    var newX = Math.floor((Math.random() * viewWidth)  - boxWidth);
+    var newY = Math.floor((Math.random() * viewHeight)  - boxHeight);
+    console.log(newX);
+    console.log(newY);
+
+    // These will satisfy that box does not move go out in the top and left direction
+    if(newX < 0) {
+        newX = 0;
+    }
+    if(newY < 0) {
+        newY = 0;
+    }
+    
+    return {x: newX, y: newY};
+     console.log(newX);
+    console.log(newY);
+}
